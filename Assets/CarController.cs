5835eb947ca36a3a7cba9f7584bc77f06752f848
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-05-02 19:58:00 +0900

    CarController

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     float speed = 0;
+    Vector2 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +16,21 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // マウスクリック
+        // スワイプの長さを求める
         if (Input.GetMouseButtonDown(0))
         {
-            this.speed = 0.2f;  // 初速度を設定
+            // マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+            Debug.Log(this.startPos);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            // スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 5000.0f;
         }
         transform.Translate(this.speed, 0, 0);  // 移動
         this.speed *= 0.98f; // 減速
